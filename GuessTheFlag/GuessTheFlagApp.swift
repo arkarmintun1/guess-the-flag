20bf83e1be63f3473102434c8ccdb8241588aa68
Arkar Min Tun <arkarmintun1@gmail.com>
2023-08-17 00:01:03 +0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Arkar Min Tun on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
