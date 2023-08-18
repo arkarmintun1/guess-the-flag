f5cf05fa7a448802367c6c4ccda19e1f78f50ef3
Arkar Min Tun <arkarmintun1@gmail.com>
2023-08-18 21:04:30 +0800

    :sparkles: Implemented guess the flag application

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var currentQuestionCount = 0;
+    
+    private var maxQuestionCount = 8;
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+
+            RadialGradient(stops: [
+                .init(color: Color(red:0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red:0.66, green: 0.15, blue: 0.36), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                
+                VStack (spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            if currentQuestionCount < maxQuestionCount {
+                Button("Continue", action: askQuestion)
+            } else {
+                Button("Reset", action: resetGame)
+            }
+        } message: {
+            if currentQuestionCount < maxQuestionCount {
+                Text("Your current score: \(score)")
+            } else {
+                Text("Your final score: \(score)")
+            }
+            
+        }
+    }
+   
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+        }
+        currentQuestionCount += 1
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        score = 0
+        currentQuestionCount = 0
+        askQuestion()
     }
 }
 
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Guess The Flag
+
+The application to quiz our knowledge about flags. I created this application as part of learning iOS development with SwiftUI.
+
+[![Watch the video](https://img.youtube.com/vi/jHRVO59mif8/hqdefault.jpg)](https://youtube.com/shorts/jHRVO59mif8)
+
+## Tech Stack
+
+- SwiftUI
+- Swift
+
+## How it's built?
+
+- The application uses `@State` for local state management for a number of values like countries, current score, random number, etc. Those values are two-way binding.
+- The UI uses various combination of ZStack, VStack, Button, Image, RadientGradient with modifiers.
